3a3a1fcda8fab0497257c5d3c52c55d793b99588
UkritS <112687265+UkritS@users.noreply.github.com>
2022-09-02 15:42:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st # pip install streamlit
 import pandas as pd # pip install pandas
 #import plotly.express as px # pip install plotly-express
 #import plotly.figure_factory as ff
+import plotly
 from plotly.tools import FigureFactory as ff
 import base64 # Standard Python Module
 from io import StringIO, BytesIO #Standard Python Module
